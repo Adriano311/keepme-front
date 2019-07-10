0d72bb46a0594d42fefd8827cceda76e6e13e5ac
Adriano311 <42031062+Adriano311@users.noreply.github.com>
2019-07-10 13:21:14 +0200

    Update index.js

--- config/index.js
+++ config/index.js
@@ -13,9 +13,10 @@ module.exports = {
     proxyTable        : {},
 
     // Various Dev Server settings
-    host           : 'localhost', // can be overwritten by process.env.HOST
+    host           : '0.0.0.0', // can be overwritten by process.env.HOST
     port           : 4000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
+    disableHostCheck: true,
     errorOverlay   : true,
     notifyOnErrors : true,
     poll           : false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
